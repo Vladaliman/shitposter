4bffcf372de1dd3e31041af37a9c7bf8937ce736
VladimirOstojic <vladaliman@gmail.com>
2020-05-24 21:06:42 +0200

    added createScream and getScream functions

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,45 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+
+admin.initializeApp();
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
   response.send("Hello from Firebase!");
+});
+
+exports.getScreams = functions.https.onRequest((req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push(doc.data());
+      });
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
+
+exports.createScream = functions.https.onRequest((req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({ error: `${req.method} method not allowed` });
+  }
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: admin.firestore.Timestamp.fromDate(new Date()),
+  };
+  admin
+    .firestore()
+    .collection("screams")
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} has been created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "something went wrong on the server" });
+      console.error(err);
+    });
 });
